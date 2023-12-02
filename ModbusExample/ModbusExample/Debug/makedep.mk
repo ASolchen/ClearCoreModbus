b5efb3295ca34921727efb1a47b8eceb2d39b7fa
Adam Solchenberger <asolchenberger@gmail.com>
2023-12-01 19:52:24 -0600

    main.cpp builds

---
+++ ModbusExample/ModbusExample/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Device_Startup\startup_same53.c
+
+Device_Startup\system_same53.c
+
+main.cpp
+
